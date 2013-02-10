840e26d89c7df7ff3cfa37d6f97adeaaa44bc6ed
Christopher Scott Hernandez <c.scott.hernandez@gmail.com>
2013-02-10 12:01:51 -0800

    added .inputrc to linked files

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,16 +31,15 @@ function safe_link() {
   ln -s "$2" "$1"
 }
 
-# symlink .bash* files
 safe_link "${HOME}/.bashrc" "${DOTFILES_DIR}/.bashrc"
 safe_link "${HOME}/.bash_profile" "${DOTFILES_DIR}/.bash_profile"
+safe_link "${HOME}/.inputrc" "${DOTFILES_DIR}/.inputrc"
+safe_link "${HOME}/.dotfiles" "${DOTFILES_DIR}"
 
 # stash any old copies of .profile
 # .profile is only used for backwards compatibility anyway
 safe_remove "${HOME}/.profile"
 
-# symlink dotfiles directory to ~/.dotfiles
-safe_link "${HOME}/.dotfiles" "${DOTFILES_DIR}"
 
 source ~/.bash_profile
 
